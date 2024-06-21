960afecd602942aa5813940bdf464d20b2ab6f48
frojas298 <fernando_rojasm@outlook.com>
2024-06-21 16:24:42 -0600

    prenatal_care_page modified

--- lib/prenatal_care_page.dart
+++ lib/prenatal_care_page.dart
@@ -54,16 +54,25 @@ class PrenatalCareCard extends StatelessWidget {
   }
 }
 
-class PrenatalCareDetailsPage extends StatelessWidget {
+class PrenatalCareDetailsPage extends StatefulWidget {
   final String weekRange;
 
   const PrenatalCareDetailsPage({required this.weekRange, super.key});
 
   @override
+  _PrenatalCareDetailsPageState createState() => _PrenatalCareDetailsPageState();
+}
+
+class _PrenatalCareDetailsPageState extends State<PrenatalCareDetailsPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _imc;
+  String? _talla;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalles Semana $weekRange'),
+        title: Text('Detalles Semana ${widget.weekRange}'),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
@@ -73,42 +82,122 @@ class PrenatalCareDetailsPage extends StatelessWidget {
       ),
       body: Container(
         color: Colors.white, // Asegura el fondo blanco
-        child: ListView(
+        child: Padding(
           padding: const EdgeInsets.all(8.0),
-          children: <Widget>[
-            const Text(
-              'Exámenes clínicos o laboratorios',
-              style: TextStyle(fontWeight: FontWeight.bold),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                const Text(
+                  'Exámenes clínicos o laboratorios',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                DataTable(columns: const [
+                  DataColumn(label: Text('Tipo')),
+                  DataColumn(label: Text('Fecha')),
+                  DataColumn(label: Text('Resultado')),
+                ], rows: const [
+                  DataRow(cells: [
+                    DataCell(Text('Examen de sangre')),
+                    DataCell(Text('15/06/2023')),
+                    DataCell(Text('Normal')),
+                  ]),
+                ]),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Visitas ginecológicas',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                // Añadir más detalles de visitas ginecológicas
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Exámenes Físicos',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'IMC',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese un valor';
+                    }
+                    final n = num.tryParse(value);
+                    if (n == null) {
+                      return 'Por favor ingrese un valor numérico';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _imc = value,
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Talla',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor ingrese un valor';
+                    }
+                    final n = num.tryParse(value);
+                    if (n == null) {
+                      return 'Por favor ingrese un valor numérico';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _talla = value,
+                ),
+                const SizedBox(height: 16.0),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Éxito'),
+                              content: const Text('Formulario enviado de manera exitosa'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Text('Enviar'),
+                  ),
+                ),
+              ],
             ),
-            DataTable(columns: const [
-              DataColumn(label: Text('Tipo')),
-              DataColumn(label: Text('Fecha')),
-              DataColumn(label: Text('Resultado')),
-            ], rows: const [
-              DataRow(cells: [
-                DataCell(Text('Examen de sangre')),
-                DataCell(Text('15/06/2023')),
-                DataCell(Text('Normal')),
-              ]),
-            ]),
-            const SizedBox(height: 16.0),
-            const Text(
-              'Visitas ginecológicas',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            // Añadir más detalles de visitas ginecológicas
-            const SizedBox(height: 16.0),
-            const Text(
-              'Exámenes Físicos',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'IMC'),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Talla'),
-            ),
-          ],
+          ),
         ),
       ),
     );
